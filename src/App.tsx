db28a09846b3fdb5fb45432a78a3878290f367d0
Shashank Vishwakarma <150128068+shashank-vishwakarma-rl@users.noreply.github.com>
2024-05-10 12:05:41 +0530

    creating reusable components using Props and props destructing

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import './App.css'
 // @ts-ignore
-import reactImg from './assets/react-core-concepts.png'
+import reactImg from './assets/react-core-concepts.png';
+// @ts-ignore
+import {CORE_CONCEPTS} from './data.js';
 
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core']
 
@@ -22,11 +24,31 @@ function Header() {
   )
 }
 
+function CoreConcept({image, title, description}) {
+  return (
+      <li>
+        <img src={image} alt={image}/>
+        <h3>{title}</h3>
+        <p>{description}</p>
+      </li>
+  )
+}
+
 function App() {
   return (
       <div>
         <Header/>
         <main>
+          <section id="core-concepts">
+            <h2>Core Concepts</h2>
+            <ul>
+              <CoreConcept title={CORE_CONCEPTS[0].title} description={CORE_CONCEPTS[0].description}
+                           image={CORE_CONCEPTS[0].image}/>
+              <CoreConcept {...CORE_CONCEPTS[1]}/>
+              <CoreConcept {...CORE_CONCEPTS[2]}/>
+              <CoreConcept {...CORE_CONCEPTS[3]}/>
+            </ul>
+          </section>
           <h2>Time to get started!</h2>
         </main>
       </div>
