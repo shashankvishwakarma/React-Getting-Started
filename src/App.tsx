323d1581d23407022e86bbff893fd069254f9265
Shashank Vishwakarma <150128068+shashank-vishwakarma-rl@users.noreply.github.com>
2024-05-10 11:40:04 +0530

    creating and using first custom component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import './App.css'
+// @ts-ignore
+import reactImg from './assets/react-core-concepts.png'
+
+const reactDescriptions = ['Fundamental', 'Crucial', 'Core']
+
+function genRandomInt(max: number) {
+  return Math.floor(Math.random() * (max + 1));
+}
 
 function Header() {
+  let description = reactDescriptions[genRandomInt(2)];
   return (
       <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom"/>
+        <img src={reactImg} alt="Stylized atom"/>
         <h1>React Essentials</h1>
         <p>
-          Fundamental React concepts you will need for almost any app you are
+          {description} React concepts you will need for almost any app you are
           going to build!
         </p>
       </header>
