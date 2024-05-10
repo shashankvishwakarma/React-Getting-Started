2a83dbd3fede2014a7045aef05f30829b02adeee
Shashank Vishwakarma <150128068+shashank-vishwakarma-rl@users.noreply.github.com>
2024-05-10 19:29:07 +0530

    adding events

---
+++ src/components/TabButton/TabButton.tsx
@@ -0,0 +1,10 @@
+// @ts-ignore
+export default function TabButton({children, onSelect, isSelected}) {
+  return (
+      <li>
+        <button className={isSelected ? 'active' : undefined} onClick={onSelect}>
+          {children}
+        </button>
+      </li>
+  )
+}
